a4b6e1e53103504b35d19f24a80c1e830bf468db
DevByAndrei <devbyandrei@gmail.com>
2025-11-06 13:52:34 +0100

    fix: changed the css import

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { HeroUIProvider } from "@heroui/react";
 import { HelmetProvider } from "react-helmet-async";
-import "./styles/index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
